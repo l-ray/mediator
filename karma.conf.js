0b5e46ffb2f36ae32d87568f366a390073c2cb5a
L-Ray <lennart_reuther@web.de>
2014-05-31 17:19:22 +0100

    adds support for travis ci

--- karma.conf.js
+++ karma.conf.js
@@ -91,7 +91,7 @@ module.exports = function(config) {
 
     // Continuous Integration mode.
     // If it's `true`, then it captures browsers, runs the tests and exits.
-    singleRun : false
+    singleRun : true
 
   });
 
