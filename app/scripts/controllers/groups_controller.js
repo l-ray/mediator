d608af5d9fb68fda82d269e471d4bdb1841980c3
L-Ray <lennart_reuther@web.de>
2014-07-19 18:11:14 +0100

    cleans up group controller through removing unused functions

--- app/scripts/controllers/groups_controller.js
+++ app/scripts/controllers/groups_controller.js
@@ -2,25 +2,26 @@ Mediator.GroupsController = Ember.ArrayController.extend({
 
     sortProperties: ['priority','title'],
     sortAscending: false,
-
-     markRecycled: function(item) {
-         item.set("recycled", true);
-     },
-     markRestored: function(item) {
-         item.set("recycled", false);
-     },
-     decreaseUserPriority: function(item) {
-         item.set(
-             "priorityByUser",
-             item.get("priorityByUser") - 1000
-         )
-     },
-     increaseUserPriority: function(item) {
-         item.set(
-             "priorityByUser",
-             item.get("priorityByUser") + 1000
-         )
-     }
-
+    actions: {
+        markRecycled: function (item) {
+            item.set("recycled", true);
+        },
+        markRestored: function (item) {
+            item.set("recycled", false);
+        },
+        decreaseUserPriority: function (item) {
+            console.log("groups controller")
+            item.set(
+                "priorityByUser",
+                    item.get("priorityByUser") - 1000
+            )
+        },
+        increaseUserPriority: function (item) {
+            item.set(
+                "priorityByUser",
+                    item.get("priorityByUser") + 1000
+            )
+        }
+    }
 });
 
