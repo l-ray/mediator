d69c4edf18e912f47e74e8cbb563a22846e5e593
L-Ray <lennart_reuther@web.de>
2015-05-03 23:43:43 +0100

    groupset calendar now auto-generated

--- app/scripts/adapters/groupset_adapter.js
+++ app/scripts/adapters/groupset_adapter.js
@@ -3,45 +3,78 @@
      // generate the complete groupset for the given ID including
      // connections
      find: function(store, type, id) {
-         var fixtures = this.fixturesForType(type),
-             fixture;
 
-         console.log("groupSet find");
+         var splitDate = id.split("-")
 
-         var startDate = id;
+         var newGroupset = store.createRecord('groupset', {
+             date : new Date(splitDate[0],splitDate[1],splitDate[2]),
+             id: id
+         });
 
-        console.log(store.find('connection',{ "startDate": startDate }));
+         console.log("Creating Groupset "+splitDate)
 
-         Ember.assert("Unable to find fixtures for model type "+type.toString(), fixtures);
+         // fill up with connections
+         var promiseArray = store.find('connection',{ 'startDate': id });
 
-         if (fixtures) {
-             fixture = Ember.A(fixtures).findProperty('id', id);
-         }
+         promiseArray.then(
+             function(connections){
+                 console.log("Got the following connections:"+connections);
 
-         if (fixture) {
-             return this.simulateRemoteCall(function() {
-                 return fixture;
-             }, this);
-         }
+                 connections.forEach(
+                     function(connection) {
+
+                         newGroupset.get('connections').then(function(n) {
+                             console.log("pushed connection |"+connection+"| with source |"+connection.get('source')+"| into groupset "+newGroupset);
+                             n.pushObject(connection);
+                             connection.set('groupset', newGroupset);
+                         });
+                     }
+                 )
+             }, function(data) { console.log("Could not retrieve connection."+data);}
+         );
+
+         return this.simulateRemoteCall(function() {
+             return  newGroupset
+         }, this);
      },
 
      // generate Groupsets for the next x days in advance
      findAll: function(store, type) {
 
         var fixtures = this.fixturesForType(type);
-         var results = [];
+        var results = [];
 
-        fixtures.forEach(function(item) {
+         var DAYS_INTO_THE_PAST = 2
+         var CALENDAR_SIZE = 16
 
-            var newGroupset = store.createRecord('groupset', {
-                date : item.date,
-                id: item.id
+        var firstDayOfCalendar = new Date()
+         firstDayOfCalendar.setDate(new Date().getDate() - DAYS_INTO_THE_PAST)
+
+         console.log("CReated "+firstDayOfCalendar)
+
+         var calendar = Array.apply(null, Array(CALENDAR_SIZE))
+             .map(function(cur,index){
+                 var newDay = new Date()
+                 newDay.setDate(firstDayOfCalendar.getDate()+index)
+                 return  newDay
+             }
+         );
+
+         console.log("Sure, My calendar says: "+calendar)
+
+         calendar.forEach(function(item) {
+
+             var itemId = item.toISOString().substr(0,10)
+
+             var newGroupset = store.createRecord('groupset', {
+                date : item,
+                id: itemId
             });
 
 
-            console.log("finding connections for groupset "+item.id);
+            console.log("finding connections for groupset "+itemId);
 
-            var promiseArray = store.find('connection',{ 'startDate': item.id });
+            var promiseArray = store.find('connection',{ 'startDate': itemId });
 
             promiseArray.then(
                 function(connections){
