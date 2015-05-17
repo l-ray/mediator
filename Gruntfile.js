a03aac585899552c65a43af80e7143104c4a961d
L-Ray <lennart_reuther@web.de>
2015-05-17 23:42:27 +0100

    corrects node version to comply with heroku

--- Gruntfile.js
+++ Gruntfile.js
@@ -322,7 +322,8 @@ module.exports = function (grunt) {
                             '*.{ico,txt}',
                             '.htaccess',
                             'images/{,*/}*.{webp,gif}',
-                            'styles/fonts/*'
+                            'styles/fonts/*',
+			    'api/{,*/}*'
                         ]
                     }
                 ]
